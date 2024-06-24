39cba6317f55d9a7d5c2e4ae33c831116f7478b9
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-06-24 18:04:24 +0530

    Time: 11 ms (79.68%), Space: 11.8 MB (22.13%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        new_s=s.split()
+        
+        return len(new_s)
+        
