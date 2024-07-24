316deba89f03162d09ac778ff35a6282b0197ecc
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-07-24 18:55:18 +0530

    Time: 42 ms (25.47%), Space: 11.8 MB (6.70%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+        lst=[]
+        for i in range(len(words)):
+            if x in words[i]:
+                lst.append(i)
+        return lst
+        
