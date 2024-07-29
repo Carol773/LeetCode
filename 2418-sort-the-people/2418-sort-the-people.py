81f5b982cf8241218d2f8184a96d97a1da4d3840
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-07-29 19:44:19 +0530

    Time: 83 ms (48.06%), Space: 12.2 MB (38.42%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        return [n for _, n in sorted(zip(heights, names), reverse=True)]
+        
